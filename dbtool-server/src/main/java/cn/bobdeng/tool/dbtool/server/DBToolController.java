7fca3689631a6770e9c46e69d3e5eeca24d981e9
DengZhiGuo <bobdeng@163.com>
2019-12-27 13:55:14 +0800

    refactor

--- dbtool-server/src/main/java/cn/bobdeng/tool/dbtool/server/DBToolController.java
+++ dbtool-server/src/main/java/cn/bobdeng/tool/dbtool/server/DBToolController.java
@@ -2,8 +2,10 @@ package cn.bobdeng.tool.dbtool.server;
 
 import cn.bobdeng.tools.dbtool.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +17,10 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.sql.Types;
+import java.util.*;
 
 @RestController
 public class DBToolController implements SQLExecutor {
@@ -39,24 +41,49 @@ public class DBToolController implements SQLExecutor {
     }
 
     @GetMapping("/dbtool/export")
-    public void export(@RequestParam("table")String[] tableNames, HttpServletResponse response) throws Exception {
-        TableExporter tableExporter=new TableExcelExporter(Arrays.asList(tableNames));
+    public void export(@RequestParam("table") String[] tableNames, HttpServletResponse response) throws Exception {
+        TableExporter tableExporter = new TableExcelExporter(Arrays.asList(tableNames));
         response.setContentType("application/vnd.ms-excel");
         response.setHeader("Content-Disposition", "attachment; filename=\"export.xlsx\"");
         tableExporter.export(response.getOutputStream());
     }
 
-    public void executeSql(String sql,Object[] values) {
-        jdbcTemplate.update(sql,values);
+    public void executeSql(String sql, Object[] values) {
+        jdbcTemplate.update(sql, values);
     }
 
     @Override
     public List<TableField> getTableFields(String tableName) {
-        return null;
+        List<TableField> result = new ArrayList<>();
+        jdbcTemplate.query("select * from " + tableName, (ResultSetExtractor<Integer>) resultSet -> {
+            ResultSetMetaData rsmd = resultSet.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                TableField column = new TableField(rsmd.getColumnName(i), getColumnTypeName(rsmd, i));
+                result.add(column);
+            }
+            return columnCount;
+        });
+        return result;
+    }
+
+    private String getColumnTypeName(ResultSetMetaData rsmd, int i) throws SQLException {
+        return "string";
     }
 
     @Override
     public List<Map<String, String>> getTableRows(String tableName) {
-        return null;
+        return jdbcTemplate.query("select * from " + tableName, (RowMapper<Map<String, String>>) (resultSet, i) -> {
+            Map<String, String> values = new HashMap<>();
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            for (int j = 0; j < columnCount; j++) {
+                Object object = resultSet.getObject(j);
+                if (object != null) {
+                    values.put(metaData.getColumnName(j), object.toString());
+                }
+            }
+            return values;
+        });
     }
 }
