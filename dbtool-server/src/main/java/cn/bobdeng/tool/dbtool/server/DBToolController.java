0ccfcec3175383661476fa85990ecf7fc9225f13
DengZhiGuo <bobdeng@163.com>
2019-12-27 13:06:54 +0800

    refactor

--- dbtool-server/src/main/java/cn/bobdeng/tool/dbtool/server/DBToolController.java
+++ dbtool-server/src/main/java/cn/bobdeng/tool/dbtool/server/DBToolController.java
@@ -5,14 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class DBToolController implements SQLExecutor {
@@ -32,7 +38,25 @@ public class DBToolController implements SQLExecutor {
         return "ok";
     }
 
+    @GetMapping("/dbtool/export")
+    public void export(@RequestParam("table")String[] tableNames, HttpServletResponse response) throws Exception {
+        TableExporter tableExporter=new TableExcelExporter(Arrays.asList(tableNames));
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment; filename=\"export.xlsx\"");
+        tableExporter.export(response.getOutputStream());
+    }
+
     public void executeSql(String sql,Object[] values) {
         jdbcTemplate.update(sql,values);
+    }
+
+    @Override
+    public List<TableField> getTableFields(String tableName) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, String>> getTableRows(String tableName) {
+        return null;
     }
 }
